0558d474fa194d1e87fd63cf91906aefeb80c7b1
Jon-Michael Deldin <dev@jmdeldin.com>
2011-05-30 22:01:31 -0600

    RnaSec::CtParser::Rules (NS, docs, tests)

--- lib/ct_parser.rb
+++ lib/ct_parser.rb
@@ -2,7 +2,8 @@ require_relative 'rnasec'
 
 module RnaSec::CtParser
 
-  autoload :Record,  'ct_parser/record'
+  autoload :Record, 'ct_parser/record'
+  autoload :Rules,  'ct_parser/rules'
 
 end
 
