3b5df663bc9a7ed9bae5577cb9e321ec2ec817b7
Jon-Michael Deldin <dev@jmdeldin.com>
2011-06-15 03:17:04 -0600

    Add a parent-traversal unit test.

--- test/tree/test_internal_loop.rb
+++ test/tree/test_internal_loop.rb
@@ -15,5 +15,34 @@ class TestInternalLoop < MiniTest::Unit::TestCase
     assert_equal 'RnaSec::Tree::InternalLoop<G1-C8>', @il.to_vienna()
   end
 
+  def test_traverse_parents
+    il = RnaSec::Tree::InternalLoop.new(
+      RnaSec::Tree::BasePair.new(
+        RnaSec::Tree::Base.new(1, :g),
+        RnaSec::Tree::Base.new(8, :c),
+      )
+    )
+
+    child_il = RnaSec::Tree::InternalLoop.new(
+        RnaSec::Tree::BasePair.new(
+          RnaSec::Tree::Base.new(2, :g),
+          RnaSec::Tree::Base.new(7, :c)
+        )
+    )
+
+    child_il.children = [
+      RnaSec::Tree::BasePair.new(
+        RnaSec::Tree::Base.new(3, :g),
+        RnaSec::Tree::Base.new(4, :c)
+      )
+    ]
+
+    il << child_il
+
+    assert_equal il.object_id, il.children[0].parent.object_id
+    assert_equal child_il.object_id, child_il.children.first.parent.object_id
+    assert_equal il.object_id, child_il.children[-1].parent.parent.object_id
+  end
+
 end
 
